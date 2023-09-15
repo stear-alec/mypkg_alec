d365efec71046aa4a1cb1cbf0f8f1b08ca461713
stear-alec <alecstear@gmail.com>
2023-09-15 10:48:47 +0200

    this is my first R package !

---
+++ R/mylog.R
@@ -0,0 +1,23 @@
+#' Compute log vector
+#'
+#' @param vect a vector of length n
+#'
+#' @return returns the vector zith log compted for each element
+#'
+#' @export
+#'
+#' @examples
+#' v <- c(1, 2, 3)
+#' mylog(v)
+#'
+mylog <- function(vect) {
+  vect_log <- log(vect)
+  return(vect_log)
+}
+
+test <- c(9, 3, 12)
+mylog(test)
+
+#The package roxygen2 is very useful to document R function. It allows you to have
+#Hadley Website for more information on the package.
+
